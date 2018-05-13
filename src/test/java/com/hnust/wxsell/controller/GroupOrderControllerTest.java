93883f30a2af444de8d8c0a8d0e2419d9836b194
zzh <824497374@qq.com>
2018-05-13 12:29:24 +0800

    默认的

---
+++ src/test/java/com/hnust/wxsell/controller/GroupOrderControllerTest.java
@@ -0,0 +1,32 @@
+package com.hnust.wxsell.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Slf4j
+public class GroupOrderControllerTest {
+
+    @Test
+    public void create() {
+
+    }
+
+    @Test
+    public void list() {
+    }
+
+    @Test
+    public void detail() {
+    }
+
+    @Test
+    public void cancel() {
+    }
+}
--- src/test/java/com/hnust/wxsell/service/impl/PayServiceImplTest.java
+++ src/test/java/com/hnust/wxsell/service/impl/PayServiceImplTest.java
@@ -1,5 +1,36 @@
+package com.hnust.wxsell.service.impl;
+
+import com.hnust.wxsell.dto.OrderDTO;
+import com.hnust.wxsell.service.OrderService;
+import com.hnust.wxsell.service.PayService;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
 import static org.junit.Assert.*;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Slf4j
 public class PayServiceImplTest {
+
+    @Autowired
+    private PayService payService;
+    @Autowired
+    private OrderService orderService;
+    @Test
+    public void create() throws Exception{
+        OrderDTO orderDTO = orderService.findOne("1525763810855915549");
+        payService.create(orderDTO);
+    }
+
+    @Test
+    public void refund() throws Exception{
+        OrderDTO orderDTO = orderService.findOne("1525936252982266968");
+        payService.refund(orderDTO);
+    }
 
 }
